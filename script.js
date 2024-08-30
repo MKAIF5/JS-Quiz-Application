ac5adcbc1bbd9faca06af2db1bafc50bb27fd8da
mohammed kaif <mkaif555123@gmail.com>
2024-08-31 01:56:59 +0500

    30% work completed

---
+++ script.js
@@ -0,0 +1,23 @@
+const questions = [
+    {
+        question: "Who Is The Founder Of Pakistan ?",
+        option: ["Allama Iqbal", "Fatima ALi Jinnah", "Liaquat Ali Khan", "Quaide-Azam"]
+    },
+    {
+        question: "What Is The National Animal Of Pakistan ?",
+        option: ["Lion", "Bear", "Markhor", "Horse"]
+    },
+    {
+        question: "Which Sports Is Most Commonly Play In Pakistan ?",
+        option: ["FootBall", "Cricket", "Hockey", "Table Tennis"]
+    },
+    {
+        question: "Which City Is The Capital Of Pakistan ?",
+        option: ["Karachi", "Lahore", "Islamabad", "Quetta"]
+    },
+    {
+        question: "Which One Is The Biggest Mountain In The Pakistan ?",
+        option: ["K2", "Nanga Parbat", "Mount Everest", "Makalu"]
+    },
+
+]
